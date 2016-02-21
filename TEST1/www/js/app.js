3eaab5cafccbf32de8ae50c719879ca2869760a1
Benjamin <Votterle>
2016-02-21 16:26:07 +0100

    MAJOR STEP: Tidy Code

--- TEST1/www/js/app.js
+++ TEST1/www/js/app.js
@@ -1,38 +1,17 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-//Wait until the map is loaded
-var db = null;
-
-
 angular.module('starter', ['ionic', 'ngCordova', 'ngStorage', 'starter.factories', 'leaflet-directive', 'starter.controllers'])
 
-
-.run(function ($ionicPlatform, $cordovaSQLite) { //https://blog.nraboy.com/2014/11/use-sqlite-instead-local-storage-ionic-framework/
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
+.run(function ($ionicPlatform) {
+	$ionicPlatform.ready(function () {
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             cordova.plugins.Keyboard.disableScroll(true);
-
         }
         if (window.StatusBar) {
             // org.apache.cordova.statusbar required
             StatusBar.styleDefault();
         }
-        if (window.cordova) { //http://stackoverflow.com/questions/26101120/how-do-i-use-the-ngcordova-sqlite-service-and-the-cordova-sqliteplugin-with-ioni
-            db = $cordovaSQLite.openDB({ name: "my.db", location: 2 }, successcb, errorcb); //device
-        }else{
-            db = window.openDatabase("my.db", '1', 'my', 1024 * 1024 * 100); // browser
-        }
-        $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
     });
 })
-
 
 .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
@@ -44,55 +23,6 @@ angular.module('starter', ['ionic', 'ngCordova', 'ngStorage', 'starter.factories
             controller: 'AppCtrl'
         })
 
-        .state('app.search', {
-            url: '/search',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/search.html'
-                }
-            }
-        })
-
-        .state('app.mapstest', {
-            url: '/mapstest',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/mapstest.html',
-                    controller: 'MapController'
-                }
-            }
-        })
-
-        .state('app.mapstestzwei', {
-            url: '/mapstestzwei',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/mapstestzwei.html',
-                    controller: 'MapControllerZwei'
-                }
-            }
-        })
-
-        .state('app.mapstestdrei', {
-            url: '/mapstestdrei',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/mapstestdrei.html',
-                    controller: 'MapControllerDrei'
-                }
-            }
-        })
-
-       /* .state('app.seminare', {
-            url: '/seminare',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/seminare.html',
-                    controller: 'SeminareCtrl'
-                }
-            }
-        })
-*/
         .state('app.testendesSeminar', {
             url: '/testendesSeminar',
             views: {
---
+++ notizen.js
@@ -0,0 +1,62 @@
+/*{
+	"seminare": [{
+		"seminarName": "Personalführung",
+		"anzahlLessons": 3,
+		"aktiv": true,
+		"abgeschlossen": false,
+		"lessons": [{
+			"lessonName": "Mitarbeiter kennenlernen",
+			"reihenfolge": 1,
+			"latitude": 147.92929,
+			"longitude": -123.4567,
+			"geofenceUID": "asdfsafasdfasdfsdfsd",
+			"freigeschaltet": true,
+			"bearbeitet": false,
+			"kapitelAnzahl": 1,
+			"kapitel": [{
+				"kapitelName": "Ankuscheln",
+				"reihenfolge": 1,
+				"bearbeitet": true
+			}]
+		}, {
+			"Name": "In den Arsch treten",
+			"reihenfolge": 2,
+			"latitude": 147.92929,
+			"longitude": -123.4567,
+			"geofenceUID": "asdfsafasdfasdfsd123",
+			"freigeschaltet": true,
+			"bearbeitet": false,
+			"kapitelAnzahl": 1,
+			"kapitel": [{
+				"kapitelName": "Ankuscheln",
+				"reihenfolge": 1,
+				"bearbeitet": true
+			}]
+		}, {
+			"Name": "Feuern für Fortgeschrittene",
+			"reihenfolge": 3,
+			"latitude": 147.92929,
+			"longitude": -123.4567,
+			"geofenceUID": "asdfsafasdfasdfsd567",
+			"freigeschaltet": true,
+			"bearbeitet": false,
+			"kapitelAnzahl": 1,
+			"kapitel": [{
+				"kapitelName": "Ankuscheln",
+				"reihenfolge": 1,
+				"bearbeitet": true
+			}]
+		}]
+	}]
+}
+
+
+ <leaflet defaults="map.defaults"  center="map.center" markers="map.markers" ng-if="map"></leaflet>
+
+
+ lat: 47.618052,
+		          lng: 10.710770,
+		          message: "blah",
+		          focus: true,
+				  draggable: false
+				  */
